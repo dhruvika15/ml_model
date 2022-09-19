1147bf2eeb561a8712c265608f8e4352b2996b10
Dhruvika Sharma <dhruvikasharma20@gmail.com>
2022-09-19 12:29:34 +0530

    Create ml_model_train.py

---
+++ ml_model_train.py
@@ -0,0 +1,10 @@
+import pandas
+dataset= pandas.read_csv('ml_model_training_data.csv')
+x= dataset['YearsExperience']
+y= dataset['Salary']
+x= x.values
+x= x.reshape(-1,1)
+
+from sklearn.linear_model import LinearRegression
+model= LinearRegression()
+model.fit(x,y)
